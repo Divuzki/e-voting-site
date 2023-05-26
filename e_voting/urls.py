68787016696b324264ee456558db5191fda8ae90
Divuzki <divuzki@gmail.com>
2023-05-26 18:33:33 +0100

    Finished

--- e_voting/urls.py
+++ e_voting/urls.py
@@ -24,4 +24,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('administrator/', include('administrator.urls')),
     path('voting/', include('voting.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
